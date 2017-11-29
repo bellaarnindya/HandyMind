1f031ddf26d1aabdbcb70537e2af221b335b6f9f
Panji Rimawan <panjirimawan8a@gmail.com>
2017-11-29 12:34:44 +0700

    add class text

---
+++ app/src/main/java/com/example/sabila/handymind/shapes/Text.java
@@ -0,0 +1,18 @@
+package com.example.sabila.handymind.shapes;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import com.example.sabila.handymind.Shape;
+
+/**
+ * Created by Panji Rimawan on 11/29/2017.
+ */
+
+public class Text extends Shape {
+
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
+
+    }
+}
