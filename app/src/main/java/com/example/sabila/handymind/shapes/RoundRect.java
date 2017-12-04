181ff68b87e52349e71ffcd1a556a5a3b377123b
Syukron Rifail M <syukronrifai@gmail.com>
2017-12-04 12:10:04 +0700

    Rectangle add move

--- app/src/main/java/com/example/sabila/handymind/shapes/RoundRect.java
+++ app/src/main/java/com/example/sabila/handymind/shapes/RoundRect.java
@@ -1,7 +1,9 @@
 package com.example.sabila.handymind.shapes;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 
 import com.example.sabila.handymind.Shape;
 
@@ -18,9 +20,16 @@ public class RoundRect extends Shape {
     private float width;
     private float height;
 
+    private Paint drawPaint;
+
     public RoundRect(float x, float y) {
         this.x = x;
         this.y = y;
+
+        drawPaint = new Paint();
+        drawPaint.setColor(Color.BLACK);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeWidth(4);
     }
 
     public float getX() {
@@ -59,4 +68,62 @@ public class RoundRect extends Shape {
     public void draw(Canvas canvas, Paint paint) {
         canvas.drawRoundRect(x, y, x+width, y+height, RX, RY, paint);
     }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawRoundRect(x, y, width + x, height + y, RX, RY, drawPaint);
+    }
+
+    @Override
+    public void initialMove(float touchX, float touchY){
+        xCoordsOnTouch = touchX - x;
+        yCoordsOnTouch = touchY - y;
+    }
+
+    @Override
+    public void move(float touchX, float touchY) {
+        this.x = touchX - xCoordsOnTouch;
+        this.y = touchY - yCoordsOnTouch;
+    }
+
+    @Override
+    public void finishMove(){}
+
+    @Override
+    public void drag(float touchX, float touchY) {
+        float width = touchX - this.x;
+        float height = touchY - this.y;
+
+        if (width > 0 && height > 0) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    @Override
+    public void resize(float touchX, float touchY) {
+        float width = touchX - this.x;
+        float height = touchY - this.y;
+
+        if (width > 0 && height > 0) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    @Override
+    public boolean isTouched(float touchX, float touchY) {
+        return (touchX > this.x &&
+                touchX < this.x + this.width &&
+                touchY > this.y &&
+                touchY < this.y + this.height);
+    }
+
+    @Override
+    public void setActive() {
+        drawPaint.setStrokeWidth(7);
+    }
+
+    @Override
+    public void setInactive() {drawPaint.setStrokeWidth(5); }
 }
