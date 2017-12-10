a6a60ae16dca0f183c8fe261cea7c52738ee3090
Sabila Malahati Arnindya <bellaarnindya@gmail.com>
2017-12-10 19:43:31 +0700

    zoomable, masih ngebug

--- app/src/main/java/com/example/sabila/handymind/DrawingView.java
+++ app/src/main/java/com/example/sabila/handymind/DrawingView.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import com.example.sabila.handymind.shapes.Circle;
@@ -30,6 +31,7 @@ import static android.content.ContentValues.TAG;
 
 public class DrawingView extends View {
 
+    private static final int INVALID_POINTER_ID = -1;
     private List<Shape> shapes;
     private Shape touchedShape = null;
     private Shape shapeOnCreating = null;
@@ -39,7 +41,12 @@ public class DrawingView extends View {
 
     private String textMessage;
     public Shape shape;
-    private static boolean dashedLine = false;
+    private int mActivePointerId = INVALID_POINTER_ID;
+//    private static boolean dashedLine = false;
+
+    private float scalePointX, scalePointY, mPosX, mPosY, mLastTouchX, mLastTouchY;
+    private ScaleGestureDetector mScaleDetector;
+    private float mScaleFactor = 1.f;
 
     public DrawingView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -47,6 +54,7 @@ public class DrawingView extends View {
         tool = new RectangleTool();
 
         shapes = new ArrayList<>();
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
 
 
@@ -54,10 +62,13 @@ public class DrawingView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
-
+        canvas.save();
+        canvas.scale(mScaleFactor, mScaleFactor);
         for (int i = 0; i < shapes.size(); i++) {
             shapes.get(i).draw(canvas);
         }
+        canvas.restore();
+
     }
 
     @Override
@@ -65,9 +76,14 @@ public class DrawingView extends View {
         float touchX = event.getX();
         float touchY = event.getY();
 
+        mScaleDetector.onTouchEvent(event);
+
         Log.d(TAG, "onTouchEvent: "+event.getAction());
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                final float x = (event.getX() - scalePointX)/mScaleFactor;
+                final float y = (event.getY() - scalePointY)/mScaleFactor;
+
                 for (int i = 0; i < shapes.size(); i++) {
                     if (shapes.get(i).isTouched(touchX, touchY)) {
                         touchedShape = shapes.get(i);
@@ -86,35 +102,51 @@ public class DrawingView extends View {
                     Log.i("ACTION_DOWN", "if touched == null");
                     Log.i("ACTION_DOWN", "  add shape");
 
-                    Shape newShape = tool.createShape(touchX, touchY);
+                    Shape newShape = tool.createShape(x - mPosX + scalePointX, y - mPosY + scalePointY);
                     shapes.add(newShape);
                     shapeOnCreating = newShape;
-                    if(newShape instanceof Line && dashedLine) {
-                        Log.d("DEBUG", "masuk dashed line");
-                        ((Line) shapeOnCreating).setDashedLine();
-                    }
                 }
 
-
+                mLastTouchX = x;
+                mLastTouchY = y;
+                mActivePointerId = event.getPointerId(0);
                 invalidate();
                 break;
+            }
 
-            case MotionEvent.ACTION_MOVE:
-
+            case MotionEvent.ACTION_MOVE: {
+                final int pointerIndex = event.findPointerIndex(mActivePointerId);
+                final float x = event.getX(pointerIndex);
+                final float y = event.getY(pointerIndex);
                 Log.i("ACTION_MOVE", "move");
 
                 isSingleTouch = false;
 
-                if (shapeOnCreating != null) {
-                    tool.drag(touchX, touchY);
-                } else if (touchedShape != null) {
-                    touchedShape.move(touchX, touchY);
+                if (!mScaleDetector.isInProgress()) {
+                    if (shapeOnCreating != null) {
+                        tool.drag(touchX, touchY);
+                    } else if (touchedShape != null) {
+                        touchedShape.move(touchX, touchY);
+                    }
+
+                    final float dx = x - mLastTouchX;
+                    final float dy = y - mLastTouchY;
+
+                    mPosX += dx;
+                    mPosY += dy;
+
+                    invalidate();
                 }
+                mLastTouchX = x;
+                mLastTouchY = y;
 
-                invalidate();
+//                invalidate();
                 break;
+            }
 
-            case MotionEvent.ACTION_UP:
+
+            case MotionEvent.ACTION_UP: {
+                mActivePointerId = INVALID_POINTER_ID;
                 shapeOnCreating = null;
 
                 if (isSingleTouch) {
@@ -127,6 +159,23 @@ public class DrawingView extends View {
 
                 invalidate();
                 break;
+            }
+
+
+            case MotionEvent.ACTION_POINTER_UP: {
+                final int pointerIndex = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                        >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                final int pointerId = event.getPointerId(pointerIndex);
+                if (pointerId == mActivePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mLastTouchX = event.getX(newPointerIndex);
+                    mLastTouchY = event.getY(newPointerIndex);
+                    mActivePointerId = event.getPointerId(newPointerIndex);
+                }
+                break;
+            }
         }
 
         return true;
@@ -142,6 +191,18 @@ public class DrawingView extends View {
     public void setActiveTool(Tool activeTool) {
         this.tool = activeTool;
 
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor,5.0f));
+
+            invalidate();
+            return true;
+        }
     }
 
 }
