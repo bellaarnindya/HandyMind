ae3f627670af7f99b08d002dcad0aa92ddc2277d
nafiarwhite <nafiar21@gmail.com>
2017-12-12 11:07:40 +0700

    add Caretaker

---
+++ app/src/main/java/com/example/sabila/handymind/ShapeCaretaker.java
@@ -0,0 +1,14 @@
+package com.example.sabila.handymind;
+
+import java.util.List;
+
+/**
+ * Created by nafiar on 12/12/17.
+ */
+
+public class ShapeCaretaker {
+    private List<ShapeMemento> listOfShapeMemento;
+
+    
+
+}
